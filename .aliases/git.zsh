6f35e987bdd99a414e37317d6bea32b8e7435b42
Ollivanders <ollivander64@gmail.com>
2025-05-06 22:13:22 +0100

    update

---
+++ .aliases/git.zsh
@@ -0,0 +1,7 @@
+# for al in $(git --list-cmds=alias); do
+    # alias "g$al"="git $al"
+# done
+
+alias gsl="git status"
+alias gs="git status -sb"
+alias gclo="git clone"
---
+++ .aliases/load.zsh
@@ -0,0 +1,21 @@
+eval "$(atuin init zsh)"
+eval "$(zoxide init zsh)"
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+
+# bun completions
+[ -s "/Users/ollivander/.bun/_bun" ] && source "/Users/ollivander/.bun/_bun"
+
+
+# nmm
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+[ -s "/Users/ollivander/.bun/_bun" ] && source "/Users/ollivander/.bun/_bun"
