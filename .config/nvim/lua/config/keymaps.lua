f1eb8908003bf3202deb76589b9e94cc3acb9e1b
Oli <oliver.baxandall@talostrading.com>
2025-06-23 16:59:33 +0100

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -187,7 +187,7 @@ end, { desc = "Search Replace within range " })
 map({ "n", "x" }, "<leader>rd", function()
   close_toggle_instance()
   local dir_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h:t")
-  local search = ".*" .. dir_name .. '"'
+  local search = "source .*=.*" .. dir_name .. '"'
   require("grug-far").toggle_instance(vim.tbl_deep_extend("force", grug_far_open_cfg, {
     prefills = {
       flags = "",
@@ -211,15 +211,15 @@ require("goto-preview").setup({
     telescope = require("telescope.themes").get_dropdown({ hide_preview = false }),
   },
   -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
-  focus_on_open = true, -- Focus the floating window when opening it.
-  dismiss_on_move = false, -- Dismiss the floating window when moving the cursor.
-  force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
-  bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
-  stack_floating_preview_windows = true, -- Whether to nest floating windows
-  same_file_float_preview = true, -- Whether to open a new floating window for a reference within the current file
+  focus_on_open = true,                                        -- Focus the floating window when opening it.
+  dismiss_on_move = false,                                     -- Dismiss the floating window when moving the cursor.
+  force_close = true,                                          -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
+  bufhidden = "wipe",                                          -- the bufhidden option to set on the floating window. See :h bufhidden
+  stack_floating_preview_windows = true,                       -- Whether to nest floating windows
+  same_file_float_preview = true,                              -- Whether to open a new floating window for a reference within the current file
   preview_window_title = { enable = true, position = "left" }, -- Whether to set the preview window title as the filename
-  zindex = 1, -- Starting zindex for the stack of floating windows
-  vim_ui_input = true, -- Whether to override vim.ui.input with a goto-preview floating window
+  zindex = 1,                                                  -- Starting zindex for the stack of floating windows
+  vim_ui_input = true,                                         -- Whether to override vim.ui.input with a goto-preview floating window
 })
 
 map("n", "<leader>th", ":Telescope harpoon marks", { desc = "Harpoon telescope" })
