c220f14931b07306aeeabfe17c9dcfa4136f636e
Oli <oliver.baxandall@talostrading.com>
2025-07-07 16:22:03 +0100

    update

--- .aliases/teleport.zsh
+++ .aliases/teleport.zsh
@@ -3,6 +3,7 @@ alias ttoken="tctl tokens add --type=node"
 alias tshs="tsh ls --search"
 alias tkill="export PROCCESSES=\$(ps -ef | grep 'tsh proxy ssh' | grep -v 'grep tsh proxy ssh' | awk '{print \$2}'); kill \$PROCCESSES ; unset PROCCESSES"
 alias tshf="tsh ls | fzf > selected | cut -d' ' -f1 | pbcopy"
+alias tshd="tsh ls -v | fzf -m"
 
 function tshl() {
   CLOUD_PROVIDER=""
