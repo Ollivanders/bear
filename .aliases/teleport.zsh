49a45cae5661d1d6867dee92fe96b457d57dbda5
Oli <oliver.baxandall@talostrading.com>
2025-08-15 13:25:36 +0100

    update

--- .aliases/teleport.zsh
+++ .aliases/teleport.zsh
@@ -38,7 +38,7 @@ function tshl() {
 function tssh() {
   echo "if unsuccessful, run: tsh ssh $1"
   echo "ssh ${USER}@${1}.${TELEPORT_HOST}  -A"
-  ssh -t ${USER}${1}.${TELEPORT_HOST} -A 'bash -o vi'
+  ssh -t ${USER}@${1}.${TELEPORT_HOST} -A 'bash -o vi'
   if [[ $2 = "c" ]]; then
     echo "tsh ssh ${USER}@$1"
     tsh ssh -A ${USER} 'bash -o vi'
